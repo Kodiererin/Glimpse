5dae7bb2d5cda0e9a29df9dbcea252c368dd57a0
Kodiererin <iamujjwal45@gmail.com>
2023-04-23 03:26:27 +0530

    Adding Files -2

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        santoshi: ['Sansothi','sans-serif'],
+        inter : ['inter','sans-serif'],
+      }
+    },
+
+  },
+  plugins: [],
+}

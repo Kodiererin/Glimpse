93014dca1387133809c0e15cdaae80cf137ef916
Kodiererin <iamujjwal45@gmail.com>
2023-04-24 13:00:20 +0530

    Adding Files

--- vite-project/src/services/article.js
+++ vite-project/src/services/article.js
@@ -2,6 +2,7 @@
 
 import {createApi , fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 
@@ -18,7 +19,15 @@ export const articleApi = createApi({
     }),
     endpoints : (builder)=>({
         getSummary : builder.query({
-            query:(params) => 'test'
+            query:(params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+            // length can be modified from 3 to many
         })
     })   
 });
+
+// The string might contain special characters so for that we must use : encodeURIComponent 
+// This is for Special Characters.
+
+export const { useLazyGetSummaryQuery } = articleApi;
+
+// Exporting this Hook.

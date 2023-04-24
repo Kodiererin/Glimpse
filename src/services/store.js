aaa9ecf3042ffe1a2c9c898f8b929aff89f0660f
Kodiererin <iamujjwal45@gmail.com>
2023-04-24 22:01:32 +0530

    Adding Files

---
+++ src/services/store.js
@@ -0,0 +1,15 @@
+import {configureStore } from '@reduxjs/toolkit'
+
+import { articleApi } from './article'
+
+export const store = configureStore({
+    reducer : {
+        [articleApi.reducerPath] : articleApi.reducer
+    },
+    middleware: (getDefaultMiddleware)=>{
+       return getDefaultMiddleware().concat(articleApi.middleware)
+    }
+})
+
+// Reducer Will allow to get the soecific slice.
+// Redux Docs...... 

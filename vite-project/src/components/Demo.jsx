c0c4ad6ab0b0789d42ab7b4cab7bc73bc0225136
Kodiererin <iamujjwal45@gmail.com>
2023-04-24 07:57:05 +0530

    Adding Files

--- vite-project/src/components/Demo.jsx
+++ vite-project/src/components/Demo.jsx
@@ -4,13 +4,31 @@ import { useState, useEffect } from 'react'
 import {copy,linkIcon,loader,tick} from "../assets";
 
 const Demo = () => {
+
+  const[article,setArticle] = useState({
+    url:'',
+    summary:''
+  });
+
+  const handleSubmit = async (e) =>{
+    alert("Submitted");
+  }
+
   return (
     <section className='mt-16 w-full max-w-xl' >
         {/* Search Component */}
         <div className='flex flex-col w-full gap-2' >
           <form 
             className='relative flex justify-center items-center'
-            onSubmit={()=>{}}
+            // onSubmit={()=>{handleSubmit}}
+            onSubmit={handleSubmit}
+            // We will do Proper AI GPT Request.
+            // In Future.
+            // https://www.youtube.com/watch?v=vpvtZZi5ZWk
+            // https://github.com/adrianhajdin/project_ai_summarizer/tree/main/src
+            // https://rapidapi.com/restyler/api/article-extractor-and-summarizer?utm_source=youtube.com%2FJavaScriptMastery&utm_medium=referral&utm_campaign=DevRel
+            // https://vitejs.dev/guide/
+            // https://tailwindcss.com/docs/guides/vite
           >
             <img src={linkIcon}
             alt="Icon"
@@ -18,8 +36,8 @@ const Demo = () => {
             <input 
               type='url'
               placeholder='Enter a URL'
-              value=""
-              onChange={()=>{}}
+              value={article.url}
+              onChange={(e)=>setArticle({...article,url:e.target.value})}
               required
               className='url_input peer'
             />
